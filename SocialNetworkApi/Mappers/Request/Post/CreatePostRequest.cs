020aad3e2ad7aa5c80803c112db6f4a7dbe5908e
gabygabylagaby <gabrielagv7656@gmail.com>
2024-10-02 14:03:21 -0400

    fix: solved request in user

--- SocialNetworkApi/Mappers/Request/Post/CreatePostRequest.cs
+++ SocialNetworkApi/Mappers/Request/Post/CreatePostRequest.cs
@@ -2,17 +2,17 @@ using SocialNetwork.Entities;
 using SocialNetwork.Persistence.DataBase;
 
 namespace SocialNetwork.Mappers.Requests;
-public record CreatePostRequest(string Content, string ImageUrl)
+public record CreatePostRequest(Guid UserId, string Content, string ImageUrl)
 {
     public Post ToDomain()
     {
         return new Post
         {
             Id = Guid.NewGuid(),
+            UserId = UserId,
             Content = Content,
             ImageUrl = ImageUrl,
             CreatedAt = DateTime.UtcNow,
-            UserId = UserContext.CurrentUserId
         };
     }
 }
