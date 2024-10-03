af582cad23f24c2e6cbdb95513f3a518feb16aa3
gabygabylagaby <gabrielagv7656@gmail.com>
2024-10-03 02:36:30 -0400

    feat: add request in login

---
+++ SocialNetworkApi/Mappers/Request/Auth/LoginRequest.cs
@@ -0,0 +1,8 @@
+namespace SocialNetworkApi.Mappers.Request.Auth;
+
+public class LoginRequest
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
+
---
+++ SocialNetworkApi/Mappers/Request/Auth/RegisterRequest.cs
@@ -0,0 +1,9 @@
+namespace SocialNetworkApi.Mappers.Request.Auth;
+public class RegisterRequest
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string ProfilePicture { get; set; }
+}
+
