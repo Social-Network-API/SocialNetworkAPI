d1b1959d6605596d04c04a6e46d2fce0ea3bad07
gabygabylagaby <gabrielagv7656@gmail.com>
2024-10-03 00:38:54 -0400

    feat: implement like entity

---
+++ SocialNetworkApi/DataAccess/Entities/Like.cs
@@ -0,0 +1,14 @@
+namespace SocialNetwork.Entities;
+public class Like
+{
+    public Guid Id { get; set; }
+    public Guid PostId { get; set; }
+    public Guid UserId { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public Like()
+    {
+        Id = Guid.NewGuid();
+        CreatedAt = DateTime.UtcNow;
+    }
+}
