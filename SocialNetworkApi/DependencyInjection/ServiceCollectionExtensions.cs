d94bb4c79bfc412284a7fc6c62fb59d2a15122de
gabygabylagaby <gabrielagv7656@gmail.com>
2024-10-03 01:06:35 -0400

    feat: add services and repository

---
+++ SocialNetworkApi/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+namespace SocialNetworkApi.DependencyInjection;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SocialNetwork.Persistence.DataBase;
+using SocialNetwork.Persistence.Repositories;
+using SocialNetwork.Services;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddSocialNetworkServices(this IServiceCollection services)
+    {
+        services.AddScoped<PostsService>();
+        services.AddScoped<UserService>();
+        services.AddScoped<CommentsService>();
+        return services;
+    }
+
+    public static IServiceCollection AddSocialNetworkRepositories(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<UserRepository>();
+        services.AddScoped<CommentsRepository>();
+        services.AddScoped<PostsRepository>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddSocialNetworkDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+        return services;
+    }
+}
