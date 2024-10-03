25107f110c0c3336ed13f0768fb95a46ef5c1ffc
gabygabylagaby <gabrielagv7656@gmail.com>
2024-10-03 00:43:49 -0400

    feat: add like service and repository in bd

--- SocialNetworkApi/Persistence/DataBase/ApplicationDbContext.cs
+++ SocialNetworkApi/Persistence/DataBase/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ public class ApplicationDbContext : DbContext
     public DbSet<Friend> Friends { get; set; }
     public DbSet<User> Users { get; set; }
     public DbSet<Comment> Comments { get; set; }
+    public DbSet<Like> Likes { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
